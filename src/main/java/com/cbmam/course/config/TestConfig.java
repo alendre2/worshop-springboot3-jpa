849b41bdd39b62475340a90e30087ff36b41f3e7
Alendre Maciel <alendrek6@hotmail.com>
2024-08-19 13:17:42 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/cbmam/course/config/TestConfig.java
+++ src/main/java/com/cbmam/course/config/TestConfig.java
@@ -41,24 +41,34 @@ public class TestConfig implements CommandLineRunner {
 		Categoria cat2 = new Categoria(null, "Livros");
 		Categoria cat3 = new Categoria(null, "Computadores");
 		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+
 		Usuario u1 = new Usuario(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		Usuario u2 = new Usuario(null, "Alex Green", "alex@gmail.com", "977777777", "123456"); 
+
+		usuarioRepository.saveAll(Arrays.asList(u1, u2));
 
 		Pedido o1 = new Pedido(null, Instant.parse("2019-06-20T19:53:07Z"), StatusPedido.ESPERANDO_PAGAMENTO, u1);
 		Pedido o2 = new Pedido(null, Instant.parse("2019-07-21T03:42:10Z"), StatusPedido.PAGO, u2);
 		Pedido o3 = new Pedido(null, Instant.parse("2019-07-22T15:21:22Z"), StatusPedido.ENTREGUE, u1);
 		
+		pedidoRepository.saveAll(Arrays.asList(o1, o2, o3));
+
 		Produto p1 = new Produto(null, "O Senhor dos Anéis", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
 		Produto p2 = new Produto(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
 		Produto p3 = new Produto(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
 		Produto p4 = new Produto(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
 		Produto p5 = new Produto(null, "Rails para Iniciantes", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
-
 		
-		// Salvar no banco de dados
-		usuarioRepository.saveAll(Arrays.asList(u1, u2));
-		pedidoRepository.saveAll(Arrays.asList(o1, o2, o3));
-		categoriaRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
-		produtoRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
+		p1.getCategorias().add(cat2);
+		p2.getCategorias().add(cat1);
+		p2.getCategorias().add(cat3);
+		p3.getCategorias().add(cat3);
+		p4.getCategorias().add(cat3);
+		p5.getCategorias().add(cat2);
+		
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 	}
 }
