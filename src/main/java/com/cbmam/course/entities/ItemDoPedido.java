a319586e74e4a673229133a3f505389d21948246
Alendre Maciel <alendrek6@hotmail.com>
2024-08-19 14:19:21 -0300

    OrderItem, many-to-many association with extra attributes parte1

---
+++ src/main/java/com/cbmam/course/entities/ItemDoPedido.java
@@ -0,0 +1,84 @@
+package com.cbmam.course.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import com.cbmam.course.entities.pk.ItemDoPedidoPK;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_item_do_pedido")
+public class ItemDoPedido implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@EmbeddedId
+	private ItemDoPedidoPK id;
+	
+	private Integer quantidade;
+	private Double Preco;
+	
+	public ItemDoPedido() {
+	}
+
+	public ItemDoPedido(Pedido pedido, Produto produto, Integer quantidade, Double preco) {
+		super();
+		id.setPedido(pedido);
+		id.setProduto(produto);
+		this.quantidade = quantidade;
+		Preco = preco;
+	}
+	
+	public Pedido getPedido() {
+		return id.getPedido();
+	}
+	
+	public void setPedido(Pedido pedido) {
+		id.setPedido(pedido);
+	}
+	
+	public Produto getProduto() {
+		return id.getProduto();
+	}
+	
+	public void setPedido(Produto produto) {
+		id.setProduto(produto);
+	}
+
+	public Integer getQunatidade() {
+		return quantidade;
+	}
+
+	public void setQunatidade(Integer qunatidade) {
+		this.quantidade = qunatidade;
+	}
+
+	public Double getPreco() {
+		return Preco;
+	}
+
+	public void setPreco(Double preco) {
+		Preco = preco;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ItemDoPedido other = (ItemDoPedido) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	
+}
