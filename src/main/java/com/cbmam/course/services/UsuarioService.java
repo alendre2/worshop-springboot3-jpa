7e90461b4759422358229779be69b9fb3d52b268
Alendre Maciel <alendrek6@hotmail.com>
2024-08-19 16:59:32 -0300

    Tste

--- src/main/java/com/cbmam/course/services/UsuarioService.java
+++ src/main/java/com/cbmam/course/services/UsuarioService.java
@@ -12,16 +12,24 @@ import com.cbmam.course.repositories.UsuarioRepository;
 @Service
 public class UsuarioService {
 
-	@Autowired
-	private UsuarioRepository repositorio;
-	
-	public List<Usuario> buscarTodos(){
-		return repositorio.findAll();
-	}
-	
-	public Usuario buscarPorId(Long id) {
-		Optional<Usuario> obj = repositorio.findById(id);
-		return obj.get();
-	}
-	
+    @Autowired
+    private UsuarioRepository repositorio;
+    
+    public List<Usuario> buscarTodos(){
+        return repositorio.findAll();
+    }
+    
+    public Usuario buscarPorId(Long id) {
+        Optional<Usuario> obj = repositorio.findById(id);
+        return obj.orElse(null); // Melhor usar orElse(null) em vez de get()
+    }
+    
+    public Usuario insert(Usuario obj) {
+        System.out.println("Tentando inserir usuário: " + obj); // Log detalhado
+        Usuario salvo = repositorio.save(obj);
+        System.out.println("Usuário salvo com sucesso: " + salvo); // Log após a inserção
+        return salvo;
+    }
+
+    
 }
